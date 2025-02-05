04c8680119aaace12c25094a2d806239f3838d84
Jakup Emini <jakup.emini.5@gmail.com>
2025-02-05 21:01:07 +0100

    Added signing/register pages

--- habit_tracker/lib/main.dart
+++ habit_tracker/lib/main.dart
@@ -5,19 +5,25 @@ import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  // Set up the SettingsController, which will glue user settings to multiple
-  // Flutter Widgets.
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  FirebaseUIAuth.configureProviders([
+      EmailAuthProvider(),
+      // ... other providers
+    ]);
+
   final settingsController = SettingsController(SettingsService());
 
   // Load the user's preferred theme while the splash screen is displayed.
   // This prevents a sudden theme change when the app is first displayed.
   await settingsController.loadSettings();
+
+  
 
   // Run the app and pass in the SettingsController. The app listens to the
   // SettingsController for changes, then passes it further down to the
--- habit_tracker/lib/src/sample_feature/sample_item_details_view.dart
+++ habit_tracker/lib/src/sample_feature/sample_item_details_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 
 /// Displays detailed information about a SampleItem.
 class SampleItemDetailsView extends StatelessWidget {
@@ -12,8 +14,18 @@ class SampleItemDetailsView extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Item Details'),
       ),
-      body: const Center(
-        child: Text('More Information Here'),
+      body: Center(
+        child: SignInScreen(
+          actions: [
+            AuthStateChangeAction<SignedIn>((context, state) {
+              if (!state.user!.emailVerified) {
+                Navigator.pushNamed(context, '/verify-email');
+              } else {
+                Navigator.pushReplacementNamed(context, '/profile');
+              }
+            }),
+          ],
+        ),
       ),
     );
   }
