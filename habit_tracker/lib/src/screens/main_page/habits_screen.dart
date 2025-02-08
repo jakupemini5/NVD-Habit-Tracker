b18fc08008cb98562a394828562ec7080104a577
Jakup Emini <jakup.emini.5@gmail.com>
2025-02-08 20:09:22 +0100

    added list of habits showing

--- habit_tracker/lib/src/screens/main_page/habits_screen.dart
+++ habit_tracker/lib/src/screens/main_page/habits_screen.dart
@@ -1,12 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:habit_tracker/src/screens/habit/create_habit_screen.dart';
+import 'package:habit_tracker/src/models/habit_model.dart';
+import 'package:habit_tracker/src/widgets/habbit_card.dart';
+import 'package:habit_tracker/src/widgets/rounded_pill_button.dart';
 import '../../widgets/my_app_bar.dart';
-import '../../widgets/rounded_pill_button.dart';
 
 class HabitsScreen extends StatefulWidget {
   HabitsScreen({super.key});
 
-  final String title = "Habit tracker";
+  final String title = "Habit Tracker";
 
   @override
   State<HabitsScreen> createState() => _HabitScreenState();
@@ -14,49 +18,79 @@ class HabitsScreen extends StatefulWidget {
 
 class _HabitScreenState extends State<HabitsScreen> {
   @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: MyAppBar(title: widget.title),
       backgroundColor: Colors.black,
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0), // Add padding here
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              RoundedPillButton(
-                radius: 16,
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => CreateHabitScreen()),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            RoundedPillButton(
+              radius: 16,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => CreateHabitScreen()),
+                );
+              },
+              text: "Create Habit",
+              icon: Icons.add_circle_rounded,
+              backgroundColor: Colors.white70,
+              textColor: Colors.black87,
+              iconColor: Colors.black87,
+              width: 10000,
+            ),
+            const SizedBox(height: 20.0),
+            Expanded(
+              child: StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance
+                    .collection('habits')
+                    .where('userId', isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  }
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                  if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                    return const Center(
+                      child: Text(
+                        "No habits yet :)",
+                        style: TextStyle(
+                          color: Color.fromARGB(103, 255, 255, 255),
+                          fontWeight: FontWeight.w900,
+                          fontSize: 16,
+                        ),
+                      ),
+                    );
+                  }
+
+                  return ListView.builder(
+                    itemCount: snapshot.data!.docs.length,
+                    itemBuilder: (context, index) {
+                      var habitData = snapshot.data!.docs[index];
+                      HabitModel habit = HabitModel(
+                        userId: habitData['userId'],
+                        name: habitData['name'],
+                        description: habitData['description'],
+                        type: HabitType.values.firstWhere((type) => type.name == habitData['type']),
+                        createdAt: (habitData['createdAt'] as Timestamp?)?.toDate(),
+                        updatedAt: (habitData['updatedAt'] as Timestamp?)?.toDate(),
+                        daysOfWeek: (habitData['daysOfWeek'] as List?)?.map((e) => e as String).toList(),
+                        dayOfMonth: habitData['dayOfMonth'],
+                      );
+                      return HabitCard(habit: habit);
+                    },
                   );
                 },
-                text: "Create Habit",
-                icon: Icons.add_circle_rounded,
-                backgroundColor: Colors.white70,
-                textColor: Colors.black87,
-                iconColor: Colors.black87,
-                width: 360,
               ),
-              const SizedBox(height: 20.0), // Add spacing between button and text
-              const Text(
-                "No habits yet :)",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Color.fromARGB(103, 255, 255, 255),
-                  fontWeight: FontWeight.w900,
-                  fontSize: 16,
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
---
+++ habit_tracker/lib/src/widgets/habbit_card.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:habit_tracker/src/models/habit_model.dart';
+
+class HabitCard extends StatelessWidget {
+  final HabitModel habit;
+
+  HabitCard({required this.habit});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              habit.name,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            Text(habit.description),
+            const SizedBox(height: 10),
+            Text('Type: ${habit.type.name}'),
+            if (habit.targetNumber != null)
+              Text('Target Number: ${habit.targetNumber}'),
+            if (habit.daysOfWeek != null)
+              Text('Days of Week: ${habit.daysOfWeek!.join(', ')}'),
+            if (habit.dayOfMonth != null)
+              Text('Day of Month: ${habit.dayOfMonth}'),
+          ],
+        ),
+      ),
+    );
+  }
+}
